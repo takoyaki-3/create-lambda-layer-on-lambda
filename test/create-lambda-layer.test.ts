4a5d7fa87bb3adf57d80d93a4112df461a1c613f
takoyaki-3 <mail@takoyaki3.com>
2024-10-24 20:31:17 +0900

    Initial commit

---
+++ test/create-lambda-layer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CreateLambdaLayer from '../lib/create-lambda-layer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/create-lambda-layer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CreateLambdaLayer.CreateLambdaLayerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
